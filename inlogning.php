bed2fe9ceb5ec8533e04f140812c01952d178695
Yurii <yurii.pustovit@skola.taby.se>
2024-03-20 22:31:00 +0100

    Hashande lösenord

---
+++ inlogning.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "db-users";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "SELECT * FROM users";
+    $result = $conn->query($sql);
+
+    $login_success = false;
+    $full_name = "";
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $hash = $row["Password"];
+            if (
+                $row["Username"] == $_POST["username"] &&
+                password_verify($_POST["password"], $hash)
+            ) {
+                $login_success = true;
+                $full_name = $row["FullName"] . " ";
+            }
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+
+    if ($login_success) {
+        echo "Hej  $full_name ";
+    } else {
+        echo "Inloggning misslyckades";
+    }
+
+
+    ?>
+
+</body>
+
+</html>
